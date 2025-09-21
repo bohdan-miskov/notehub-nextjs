011c9dc009e9103135d57511455859684eff4ff7
Bohdan Miskov <bohdanmiskov@gmail.com>
2025-09-21 21:59:28 +0300

    Finish NoteDetailsPage

--- components/NoteDetailsClient/NoteDetailsClient.tsx
+++ components/NoteDetailsClient/NoteDetailsClient.tsx
@@ -1,16 +1,39 @@
 'use client';
 
+import { useParams } from 'next/navigation';
 import css from './NoteDetailsClient.module.css';
+import { useQuery } from '@tanstack/react-query';
+import { getNoteById } from '@/lib/api';
 
 export default function NoteDetailsClient() {
+  const { id } = useParams<{ id: string }>();
+
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['note', id],
+    queryFn: () => getNoteById(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error || !note) return <p>Some error..</p>;
+
+  const formattedDate = note.updatedAt
+    ? `Updated at: ${note.updatedAt}`
+    : `Created at: ${note.createdAt}`;
+
   return (
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>Note title</h2>
+          <h2>{note.title}</h2>
         </div>
-        <p className={css.content}>Note content</p>
-        <p className={css.date}>Created date</p>
+        <p className={css.content}>{note.content}</p>
+        <p className={css.date}>{formattedDate}</p>
       </div>
     </div>
   );
