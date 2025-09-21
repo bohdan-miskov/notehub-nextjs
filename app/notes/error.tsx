8c033f9297843b90239666b36c61ffcc5cea0956
Bohdan <bohdanmiskov07@gmail.com>
2025-09-21 21:59:28 +0300

    Finish NoteDetailsPage

--- app/notes/error.tsx
+++ app/notes/error.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 type Props = {
   error: Error;
 };
 
 export default function Error({ error }: Props) {
-  return <p>Could not fetch the list of notes. {error.message}</p>;
+  return <p>Could not fetch the list of notes. {error?.message}</p>;
 }
