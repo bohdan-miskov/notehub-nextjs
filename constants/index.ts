009e831f6148a769eb218d8af387bcd1de9d0d4a
Bohdan Miskov <bohdanmiskov@gmail.com>
2025-10-07 22:27:21 +0300

    Add save note draft function with zustand

--- constants/index.ts
+++ constants/index.ts
@@ -1,9 +1,9 @@
 export enum TAG {
+  Todo = 'Todo',
   Work = 'Work',
   Personal = 'Personal',
   Meeting = 'Meeting',
   Shopping = 'Shopping',
-  Todo = 'Todo',
 }
 
 export const TAGS_ARRAY = Object.values(TAG);
@@ -14,3 +14,5 @@ export const PAGE_BASE_URL = 'https://notehub-nextjs.vercel.app';
 
 export const OG_IMAGE_URL =
   'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg';
+
+export const NOTE_DRAFT_KEY = 'note-draft';
