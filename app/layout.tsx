5f821584a5bb6ca4c4dc8950f0a97c34166d06c3
Bohdan <bohdanmiskov07@gmail.com>
2025-10-07 17:20:52 +0300

    Add open graph meta tags for all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+import { OG_IMAGE_URL, PAGE_BASE_URL } from '@/constants';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -19,6 +20,22 @@ export const metadata: Metadata = {
   title: 'Note Hub',
   description:
     'Create, organize, and manage your notes effortlessly with our secure online note-taking app. Access your notes from anywhere, stay organized, and keep your ideas, tasks, and reminders in one convenient place.',
+  openGraph: {
+    title: 'Note Hub',
+    description:
+      'Create, organize, and manage your notes effortlessly with our secure online note-taking app. Access your notes from anywhere, stay organized, and keep your ideas, tasks, and reminders in one convenient place.',
+    url: `${PAGE_BASE_URL}`,
+    siteName: 'NoteHub',
+    images: [
+      {
+        url: OG_IMAGE_URL,
+        width: 1200,
+        height: 630,
+        alt: 'Note Hub',
+      },
+    ],
+    type: 'article',
+  },
   icons: {
     icon: '/favicon.svg',
   },
--- constants/index.ts
+++ constants/index.ts
@@ -9,3 +9,8 @@ export enum TAG {
 export const TAGS_ARRAY = Object.values(TAG);
 
 export const MAX_PAG_COUNT = 6;
+
+export const PAGE_BASE_URL = 'https://notehub-nextjs.vercel.app';
+
+export const OG_IMAGE_URL =
+  'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg';
