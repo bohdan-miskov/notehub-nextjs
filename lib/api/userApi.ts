11119f13ca84ad0b958db66f57eb30f97fc70038
Bohdan <bohdanmiskov07@gmail.com>
2025-10-08 16:51:39 +0300

    Add user lib/api

---
+++ lib/api/userApi.ts
@@ -0,0 +1,12 @@
+import { User, UserProfile } from '@/types/auth';
+import { nextServer } from './api';
+
+export async function getMe() {
+  const response = await nextServer.get<UserProfile>('/users/me');
+  return response.data;
+}
+
+export async function updateMe(payload: User) {
+  const response = await nextServer.patch<UserProfile>('/users/me', payload);
+  return response.data;
+}
