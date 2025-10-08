ba53680492de1dc7b8ee5af011df35e3571d7973
Bohdan <bohdanmiskov07@gmail.com>
2025-10-08 22:12:54 +0300

    Add metadata to auth pages

--- app/(public routes)/sign-in/page.tsx
+++ app/(public routes)/sign-in/page.tsx
@@ -1,4 +1,29 @@
 import LoginForm from '@/components/LoginForm/LoginForm';
+import { OG_IMAGE_URL, PAGE_BASE_URL } from '@/constants';
+
+export function generateMetadata() {
+  return {
+    title: 'Login — NoteHub',
+    description:
+      'Sign in to your NoteHub account to access your notes, manage tags, and stay productive anywhere.',
+    openGraph: {
+      title: 'Login — NoteHub',
+      description:
+        'Log in to NoteHub and instantly access your secure workspace for notes and ideas. Continue where you left off.',
+      url: `${PAGE_BASE_URL}/sign-in`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: OG_IMAGE_URL,
+          width: 1200,
+          height: 630,
+          alt: 'Login — NoteHub',
+        },
+      ],
+      type: 'article',
+    },
+  };
+}
 
 export default function SignIn() {
   return (
--- app/(public routes)/sign-up/page.tsx
+++ app/(public routes)/sign-up/page.tsx
@@ -1,4 +1,29 @@
 import RegisterForm from '@/components/RegisterForm/RegisterForm';
+import { OG_IMAGE_URL, PAGE_BASE_URL } from '@/constants';
+
+export function generateMetadata() {
+  return {
+    title: 'Create Account — NoteHub',
+    description:
+      'Join NoteHub today and start organizing your ideas with powerful tools for notes, tags, and collaboration.',
+    openGraph: {
+      title: 'Create Account — NoteHub',
+      description:
+        'Create your free NoteHub account and unlock a clean, modern space for your thoughts, notes, and inspiration.',
+      url: `${PAGE_BASE_URL}/sign-up`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: OG_IMAGE_URL,
+          width: 1200,
+          height: 630,
+          alt: 'Create Account — NoteHub',
+        },
+      ],
+      type: 'article',
+    },
+  };
+}
 
 export default function SignUp() {
   return (
