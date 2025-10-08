490cba2b3f04e08fbb1ac6ca7af6ac0739f44e7b
Bohdan Miskov <bohdanmiskov@gmail.com>
2025-10-08 18:08:04 +0300

    Add AuthNavigation component and styles for this component

---
+++ components/AuthNavigation/AuthNavigation.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { logout } from '@/lib/api/authApi';
+import { useAuthStore } from '@/lib/stores/authStore';
+import css from './AuthNavigation.module.css';
+import { ActiveLink } from '../ActiveLink/ActiveLink';
+import clsx from 'clsx';
+
+type Props = {
+  activeClassName?: string;
+};
+
+export default function AuthNavigation({ activeClassName }: Props) {
+  const { isAuthenticated, user } = useAuthStore();
+
+  async function handleLogout() {
+    await logout();
+  }
+
+  return isAuthenticated ? (
+    <li>
+      <p>{user?.username}</p>
+      <button className={css.logout} onClick={handleLogout}>
+        Logout
+      </button>
+    </li>
+  ) : (
+    <>
+      <li>
+        <ActiveLink
+          href="/sign-in"
+          className={css.authLink}
+          activeClassName={clsx(activeClassName, css.activeLink)}
+        >
+          Login
+        </ActiveLink>
+      </li>
+      <li>
+        <ActiveLink
+          href="/sign-up"
+          className={css.authLink}
+          activeClassName={clsx(activeClassName, css.activeLink)}
+        >
+          Register
+        </ActiveLink>
+      </li>
+    </>
+  );
+}
