288fd049264b28e4884f5ec88c469bf1333580ad
Bohdan <bohdanmiskov07@gmail.com>
2025-10-09 18:58:53 +0300

    Add metadata and og to  profile pages

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -1,5 +1,30 @@
 import UserForm from '@/components/UserForm/UserForm';
 import css from './page.module.css';
+import { OG_IMAGE_URL, PAGE_BASE_URL } from '@/constants';
+
+export function generateMetadata() {
+  return {
+    title: 'Edit Profile — NoteHub',
+    description:
+      'Update your name or other personal details in your NoteHub profile. Manage your account easily and securely.',
+    openGraph: {
+      title: 'Edit Profile — NoteHub',
+      description:
+        'Edit your personal information and keep your NoteHub profile up to date — a safe and simple way to manage your account.',
+      url: `${PAGE_BASE_URL}/profile/edit`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: OG_IMAGE_URL,
+          width: 1200,
+          height: 630,
+          alt: 'Edit Profile — NoteHub',
+        },
+      ],
+      type: 'article',
+    },
+  };
+}
 
 export default function EditUser() {
   return (
