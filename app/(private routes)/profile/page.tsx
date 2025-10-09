9d1a3c81c9ae48550ddd6b36e77ba9b15a357550
Bohdan <bohdanmiskov07@gmail.com>
2025-10-09 18:53:11 +0300

    Add styles to  profile pages

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,24 +1,28 @@
-// app/(private routes)/profile/page.tsx
-
 import { getServerMe } from '@/lib/api/serverApi/userApi';
 import Link from 'next/link';
+import css from './page.module.css';
 
 export default async function Profile() {
   const user = await getServerMe();
 
   return (
-    <section>
-      <div>
-        <h1>My Profile</h1>
-        <Link href="/profile/edit">Edit profile</Link>
-      </div>
-      <div>
-        <h2>Name: {user.username}</h2>
-        <h2>Email: {user.email}</h2>
-        {/* <p>
-          Some description: Lorem ipsum dolor sit amet consectetur adipisicing
-          elit...
-        </p> */}
+    <section className={css.section}>
+      <div className={`container ${css.container}`}>
+        <h1 className={css.title}>My Profile</h1>
+
+        <div className={css.card}>
+          <h2 className={css.subtitle}>Account Information</h2>
+          <p className={css.field}>
+            <span className={css.label}>Username:</span> {user.username}
+          </p>
+          <p className={css.field}>
+            <span className={css.label}>Email:</span> {user.email}
+          </p>
+
+          <Link href="/profile/edit" className={css.editBtn}>
+            Edit Profile
+          </Link>
+        </div>
       </div>
     </section>
   );
