26b93bfa77e87452c7d9adc89286b3fa29284623
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-22 22:55:37 +0200

    Created some server notes requests

--- lib/api/serverApi/noteApi.ts
+++ lib/api/serverApi/noteApi.ts
@@ -1,6 +1,36 @@
 import { cookies } from 'next/headers';
 import { nextServer } from '../api';
-import { Note } from '@/types/note';
+import { Note, NoteResponse, NotesSearchParams } from '@/types/note';
+import { PER_PAGE } from '@/constants';
+
+export async function getTagsServer() {
+  const cookieStore = await cookies();
+  const response = await nextServer.get<string[]>('/notes/tags', {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return response.data;
+}
+
+export async function getNotesServer(params: NotesSearchParams = {}) {
+  params.search = params?.search?.trim().toLowerCase();
+
+  if (params.search === '') {
+    delete params.search;
+  }
+  const cookieStore = await cookies();
+  const response = await nextServer.get<NoteResponse>('/notes', {
+    params: {
+      ...params,
+      PER_PAGE,
+    },
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return response.data;
+}
 
 export async function getNoteByIdServer(id: string) {
   const cookieStore = await cookies();
