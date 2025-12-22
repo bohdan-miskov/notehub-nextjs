bea08ba502e4aa5440722e70ea05e8956adc706d
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-22 20:25:03 +0200

    Fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,57 @@
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
-import { getAuthCookies, setAuthCookies } from './utils/cookieOperations';
-import { checkServerSession } from './lib/api/serverApi/authApi';
+import {
+  getAuthCookies,
+  setAuthCookiesFromHeaders,
+} from './utils/cookieOperations';
+import { refreshTokens } from './lib/api/serverApi/authApi';
 
 const privateRoutes = ['/profile', '/notes'];
 const authRoutes = ['/sign-in', '/sign-up'];
 
 export async function middleware(request: NextRequest) {
   const cookieStore = await cookies();
+
   const { accessToken, refreshToken } = getAuthCookies(cookieStore);
 
   const { pathname } = request.nextUrl;
+
+  if (
+    pathname.startsWith('/api/auth') ||
+    pathname.includes('_next') ||
+    pathname.includes('favicon.ico')
+  ) {
+    return NextResponse.next();
+  }
+
   const isAuthRoute = authRoutes.some(route => pathname.startsWith(route));
   const isPrivateRoute = privateRoutes.some(route =>
     pathname.startsWith(route)
   );
   if (!accessToken) {
     if (refreshToken) {
-      const data = await checkServerSession();
-      const setCookie = data.headers['set-cookie'];
+      const refreshResponse = await refreshTokens();
 
-      if (setCookie) {
-        setAuthCookies(cookieStore, setCookie);
+      const setCookieHeader =
+        refreshResponse.headers &&
+        typeof refreshResponse.headers.get === 'function'
+          ? refreshResponse.headers.get('set-cookie')
+          : null;
 
-        if (isAuthRoute) {
-          return NextResponse.redirect(new URL('/', request.url), {
-            headers: {
-              Cookie: cookieStore.toString(),
-            },
-          });
-        }
-        if (isPrivateRoute) {
-          return NextResponse.next({
-            headers: {
-              Cookie: cookieStore.toString(),
-            },
-          });
-        }
+      if (setCookieHeader) {
+        setAuthCookiesFromHeaders(
+          cookieStore,
+          refreshResponse.headers['set-cookie']
+        );
+
+        const response = isAuthRoute
+          ? NextResponse.redirect(new URL('/', request.url))
+          : NextResponse.next();
+
+        response.headers.set('set-cookie', setCookieHeader.toString());
+        response.headers.set('Cookie', cookieStore.toString());
+
+        return response;
       }
     }
 
